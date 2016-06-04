92c4ec342d6284fde1622d2376debf42ba670dc5
infinitestripes <infinitestripes@gmail.com>
2016-06-04 14:07:09 -0400

    Created Unity Scene

---
+++ Unity/Yon/Assets/Scripts/playerMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+	public bool canMove;
+	public float maxSpeed;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canMove) {
+			transform.Translate(new Vector3 (0f, Input.GetAxisRaw ("Vertical") * maxSpeed * Time.deltaTime, 0f));
+			transform.Translate(new Vector3 (Input.GetAxisRaw ("Horizontal") * maxSpeed * Time.deltaTime, 0f, 0f));
+
+			anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+			anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+		}
+	}
+}
