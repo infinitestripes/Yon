d3ef8bdb15220a091ba24d87c8abd90213e7ddb2
infinitestripes <infinitestripes@gmail.com>
2016-06-05 12:54:23 -0400

    Made some updates

--- Unity/Yon/Assets/Scripts/playerMove.cs
+++ Unity/Yon/Assets/Scripts/playerMove.cs
@@ -16,11 +16,28 @@ public class playerMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (canMove) {
+			if (Input.GetAxisRaw ("Horizontal") > 0.5) {
+				anim.SetFloat ("LastMoveX", 1f);
+			}
+
+			if (Input.GetAxisRaw ("Horizontal") < -0.5) {
+				anim.SetFloat ("LastMoveX", -1f);
+			}
+			if (Input.GetAxisRaw ("Vertical") > 0.5) {
+				anim.SetFloat ("LastMoveY", 1f);
+			}
+			if (Input.GetAxisRaw ("Vertical") < -0.5) {
+				anim.SetFloat ("LastMoveY", -1f);
+			}
+
+
 			transform.Translate(new Vector3 (0f, Input.GetAxisRaw ("Vertical") * maxSpeed * Time.deltaTime, 0f));
 			transform.Translate(new Vector3 (Input.GetAxisRaw ("Horizontal") * maxSpeed * Time.deltaTime, 0f, 0f));
 
 			anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
 			anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+			anim.SetBool ("isMoving", Mathf.Abs (Input.GetAxisRaw ("Horizontal")) > 0 || Mathf.Abs (Input.GetAxisRaw ("Vertical")) > 0);
 		}
+
 	}
 }
